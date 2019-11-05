86f09bda97cc082fe684c65b1bc2bfc92a7351eb
olegmalyarenko <oi.malyarenko@gmail.com>
2019-11-05 17:06:17 +0300

    feat: add keyboard symbols

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ keyboard.id= "keyboard";
 
 document.body.append(keyboard);
 
-const keyboardKeys = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61,8,9, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93,220,20, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 222, 13,16, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47,38,16,17,18,32,37,40,39,17];
+const keyboardKeys = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61,8, ,9, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93,220, ,20, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 222, 13, ,16, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47,38,16, ,17,18,32,37,40,39,17];
 
 const keyboardKeysUp = [126, 33, 64, 35, 36, 37, 94, 38, 42, 40, 41, 95, 43,8,9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 123, 125, 124,20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 58, 34, 13,16, 90, 88, 67, 86, 66, 78, 77, 60, 62, 63,38,16,17,18,32,37,40,39,17];
 
@@ -30,15 +30,55 @@ const keyboardKeysRusCaps = [1025, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 6
 function init ( ){
     let out ="";
     for (let i =0; i<keyboardKeys.length; i++){
-        if (i == 14 || i == 28 || i == 41 || i == 54) {
+        if (i == 14 || i == 29 || i == 43 || i == 57) {
             out += '<div class=row></div>'
+        } else if (i == 13){
+            out+= '<div class="key" data="'+keyboardKeys[i]+'">Backspace</div>'
+        } else if (i == 15){
+            out+= '<div class="key" data="'+keyboardKeys[i]+'">Tab</div>' 
+        } else if (i == 28){
+            out+= '<div class="key" data="'+keyboardKeys[i]+'">/</div>' 
+        } else if (i == 30){
+            out+= '<div class="key" data="'+keyboardKeys[i]+'">Caps Lock</div>' 
+        } else if (i == 41){
+            out+= '<div class="key" data="'+keyboardKeys[i]+'">"</div>' 
+        } else if (i == 42){
+            out+= '<div class="key" data="'+keyboardKeys[i]+'">Enter</div>' 
+        } else if (i == 44 || i == 56){
+            out+= '<div class="key" data="'+keyboardKeys[i]+'">Shift</div>' 
+        } else if (i == 44 || i == 55){
+            out+= '<div class="key" data="'+keyboardKeys[i]+'">↑</div>' 
+        }else if (i == 58 || i == 64){
+            out+= '<div class="key" data="'+keyboardKeys[i]+'">Ctrl</div>' 
+        } else if (i == 59){
+            out+= '<div class="key" data="'+keyboardKeys[i]+'">Alt</div>' 
+        } else if (i == 63){
+            out+= '<div class="key" data="'+keyboardKeys[i]+'">→</div>' 
+        } else if (i == 62){
+            out+= '<div class="key" data="'+keyboardKeys[i]+'">↓</div>' 
+        }else if (i == 61){
+            out+= '<div class="key" data="'+keyboardKeys[i]+'">←</div>' 
         }
-    out+= '<div class="key" data="'+keyboardKeys[i]+'">'+String.fromCharCode(keyboardKeys[i])+'</div>';
 
+        else {
+         
+    out+= '<div class="key" data="'+keyboardKeys[i]+'">'+String.fromCharCode(keyboardKeys[i])+'</div>';
+        }  
     }
    document.querySelector("#keyboard").innerHTML = out;
 }
 
+/*function backspace() {
+    let out ="";
+    let keyboard= document.querySelector("#keyboard");
+    for ( let i=o; i<keyboard.length; i++){
+        if(i=60){
+            out += '<div class=key space><p>Space</p></div>';
+        } 
+        document.querySelector("#keyboard").innerHTML = out;
+    }
+    
+}*/
 
 
 
@@ -52,6 +92,7 @@ document.onkeypress = function (event) {
     });   
     document.querySelector('#keyboard .key[data="' +event.keyCode+ '"]').classList.add('active');
     
+    
 }
 
 document.onkeyup = function (event) { 
@@ -60,6 +101,7 @@ document.onkeyup = function (event) {
         element.classList.remove('active');
     });   
     document.querySelector('#keyboard .key[data="' +event.keyCode+ '"]').classList.add('active');
+    
     
 }
 
@@ -87,9 +129,13 @@ document.querySelector('#keyboard').childNodes[28].classList.add('slash');
 document.querySelector('#keyboard').childNodes[30].classList.add('capslock');
 
 document.querySelector('#keyboard').childNodes[42].classList.add('enter');
+document.getElementsByClassName('.enter').innerHTML = '<p>Shift</p>';
 
 document.querySelector('#keyboard').childNodes[44].classList.add('shift');
 document.querySelector('#keyboard').childNodes[56].classList.add('shift');
+
+
+
 
 
 
